ba5c36c038e21746a2d48f89184990f73a230df0
github username <github email address>
2020-02-24 11:19:39 -0600

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
     return driver;
 }
+
+function deleteFromDriverByKey(driver, key) {
+    const newDriver = Object.assign({}, driver)
+    delete newDriver[key]
+    return newDriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver 
+}
